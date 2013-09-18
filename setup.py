0478e14d6999f9e2ab1843ff0eb80622714bbff7
Stefano Parmesan <parmesan@spaziodati.eu>
2013-09-18 12:22:24 +0200

    add setup files for pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = 'dandelion',
+    packages = ['dandelion'],
+    version = '0.1',
+    description = 'Connect to the dandelion.eu API in a very pythonic way!',
+    author = 'Stefano Parmesan',
+    author_email = 'parmesan@spaziodati.eu',
+    url = 'https://github.com/armisael/python-dandelion',
+    download_url = 'https://github.com/armisael/python-dandelion/tarball/0.1',
+    keywords = ['api', 'dandelion'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers = [],
+)
