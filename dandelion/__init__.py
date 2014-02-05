67ced31c053a03c5d6433adce6b36b9ea6988433
Stefano Parmesan <parmesan@spaziodati.eu>
2014-02-05 17:51:24 +0100

    cleanup

--- dandelion/__init__.py
+++ dandelion/__init__.py
@@ -1,9 +1,6 @@
 """ main dandelion package
 """
 
-# TODO[sp] test!!!
-# TODO[sp] documentation!!! (verificare che sia sempre from dandelion import .)
-
 from dandelion.base import DandelionException, DandelionConfig
 from dandelion.datagem import Datagem
 from dandelion.datatxt import DataTXT
