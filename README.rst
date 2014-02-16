35e40b7939a0a284dfb3df945d66d1d85ecb9c6c
Stefano Parmesan <parmesan@spaziodati.eu>
2014-02-16 18:36:47 +0100

    update version and change readme

---
+++ README.rst
@@ -0,0 +1,24 @@
+[![Build Status](https://travis-ci.org/SpazioDati/python-dandelion-eu.png?branch=master)](https://travis-ci.org/SpazioDati/python-dandelion-eu)
+[![Coverage Status](https://coveralls.io/repos/SpazioDati/python-dandelion-eu/badge.png?branch=master)](https://coveralls.io/r/SpazioDati/python-dandelion-eu?branch=master)
+[![PyPi version](https://pypip.in/v/dandelion-eu/badge.png)](https://crate.io/packages/$REPO/)
+[![PyPi downloads](https://pypip.in/d/dandelion-eu/badge.png)](https://crate.io/packages/$REPO/)
+
+
+python-dandelion
+================
+
+Connect to the dandelion.eu API in a very pythonic way!
+
+
+Installation
+------------
+
+``dandelion-eu`` is available on :samp:`https://pypi.python.org/pypi/dandelion-eu/` install it simply with::
+
+    pip install dandelion-eu
+
+
+Documentation
+-------------
+
+Documentation will be available soon.
--- setup.py
+++ setup.py
@@ -3,19 +3,33 @@ try:
 except ImportError:
     from distutils.core import setup
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
-    name = 'dandelion-eu',
-    packages = ['dandelion'],
-    version = '0.1.2',
-    description = 'Connect to the dandelion.eu API in a very pythonic way!',
-    author = 'Stefano Parmesan',
-    author_email = 'parmesan@spaziodati.eu',
-    url = 'https://github.com/armisael/python-dandelion',
-    download_url = 'https://github.com/armisael/python-dandelion/tarball/0.1.2',
-    keywords = ['api', 'dandelion'],
+    name='dandelion-eu',
+    packages=['dandelion'],
+    version='0.2.0',
+    description='Connect to the dandelion.eu API in a very pythonic way!',
+    long_description=readme,
+    author='SpazioDati s.r.l.',
+    author_email='parmesan@spaziodati.eu',
+    url='https://github.com/SpazioDati/python-dandelion-eu',
+    download_url='https://github.com/SpazioDati/python-dandelion-eu/tarball/0.2.0',
+    keywords=['api', 'dandelion'],
     install_requires=[
         'requests',
         'six',
     ],
-    classifiers = [],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ),
 )
