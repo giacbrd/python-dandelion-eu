e134e5092f91480c2f0d372433e7768e4f7d01f6
Giacomo Berardi <barnets@gmail.com>
2019-08-02 15:52:21 +0200

    text parameters not mandatory

--- dandelion/datatxt.py
+++ dandelion/datatxt.py
@@ -6,19 +6,19 @@ from dandelion.base import BaseDandelionRequest
 class DataTXT(BaseDandelionRequest):
     """ class for accessing the dataTXT family
     """
-    def nex(self, text, **params):
+    def nex(self, text='', **params):
         if 'min_confidence' not in params:
             params['min_confidence'] = 0.6
         return self.do_request(
             dict(params, text=text), ('nex', 'v1')
         )
 
-    def sim(self, text1, text2, **params):
+    def sim(self, text1='', text2='', **params):
         return self.do_request(
             dict(params, text1=text1, text2=text2), ('sim', 'v1')
         )
 
-    def li(self, text, **params):
+    def li(self, text='', **params):
         return self.do_request(
             dict(params, text=text), ('li', 'v1')
         )
