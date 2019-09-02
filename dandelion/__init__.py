6f3bbfd89e2a40ffa24145cce66a78c2c7ee9df4
Giacomo Berardi <barnets@gmail.com>
2019-09-02 12:45:55 +0200

    new version again

--- dandelion/__init__.py
+++ dandelion/__init__.py
@@ -5,5 +5,5 @@ from dandelion.base import DandelionException, DandelionConfig
 from dandelion.datagem import Datagem
 from dandelion.datatxt import DataTXT
 
-__version__ = '0.3.0'
+__version__ = '0.3.2'
 default_config = DandelionConfig()
